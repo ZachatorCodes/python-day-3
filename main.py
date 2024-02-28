8933194f1ec4c74b73b243963c4281381e9d16cc
Zach Schwartz <104332807+ZachatorCodes@users.noreply.github.com>
2024-02-28 21:44:50 +0000

    Learned about elif and nested if statements

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # Day 3
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm?\n"))
+
+if height >= 120:
+  print("You are allowed to ride the rollercoaster")
+  age = int(input("What is your age?\n"))
+
+  if age < 12:
+    print("Please pay $5.")
+  elif age <= 18:
+    print("Please pay $7.")
+  elif age <= 22:
+    print("Please pay $10.")
+  else:
+    print("LOL you're old. Give me $12.")
+else:
+  print("You CANNOT ride the rollercoaster. Maybe, be taller?")
